fe1103623d6b3fda7269e7f65aab268887d0dd3f
wkershaw <wkershaw456@gmail.com>
2020-05-22 00:28:41 +0100

    Included simple vertex and fragment shaders

--- OpenGL-basics/src/Application.cpp
+++ OpenGL-basics/src/Application.cpp
@@ -2,6 +2,47 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type,const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char)); //allocate on stack dynamically
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile shader" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(std::string& vertexShader, std::string & fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER,vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -28,7 +69,49 @@ int main(void)
     }
 
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+    std::cout << "OpenGL loaded: " <<glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = { //Vertex positions for a triangle
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer; //Create a place to store the buffer ID
+    glGenBuffers(1, &buffer); //Generate a buffer and return its ID
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); //Set the current buffer in the OpenGL state machine
+    glBufferData(GL_ARRAY_BUFFER, 6 *sizeof(float),&positions,GL_STATIC_DRAW); //Add position data to the buffer
+    
+    glEnableVertexAttribArray(0); //Enable an attribute for the buffer, defined on the next line
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float),(const void*)0); //Declare the layout of the buffer data
+
+
+    std::string vertexShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 colour;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+
+    unsigned int shader = CreateShader(vertexShader,fragmentShader);
+    glUseProgram(shader);
+
+
+    //glBindBuffer(GL_ARRAY_BUFFER, 0); //Unbind the buffer
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -36,11 +119,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glBindBuffer(GL_ARRAY_BUFFER, buffer); //Bind the buffer
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -48,6 +128,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
