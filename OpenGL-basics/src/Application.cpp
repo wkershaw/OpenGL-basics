a8d4dcc4ae131972c933971271f92394882fb6a4
wkershaw <wkershaw456@gmail.com>
2020-05-25 21:43:53 +0100

    Added second object and cleaned up code

--- OpenGL-basics/src/Application.cpp
+++ OpenGL-basics/src/Application.cpp
@@ -40,21 +40,20 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     
-    glfwSwapInterval(1);
+    glfwSwapInterval(1); //Sync the buffer swapping with 1 screen update (vsync)
 
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error!" << std::endl;
     }
 
-
     std::cout << "OpenGL loaded: " <<glGetString(GL_VERSION) << std::endl;
 
     float positions[] = { //Vertex positions for a square
-              0,   0, 0.0f, 0.0f,
-            100,   0, 1.0f, 0.0f,
-            100, 100, 1.0f, 1.0f,
-              0, 100, 0.0f, 1.0f,
+            -50, -50, 0.0f, 0.0f,
+             50, -50, 1.0f, 0.0f,
+             50,  50, 1.0f, 1.0f,
+            -50,  50, 0.0f, 1.0f,
     };
 
     unsigned int indices[] = { //Inices to draw a square
@@ -62,12 +61,11 @@ int main(void)
         2,3,0
     };
 
-    GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    GLCALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)); //Enable alpha blending
     GLCALL(glEnable(GL_BLEND))
 
     glm::mat4 proj = glm::ortho(0.0f, 680.0f, 0.0f , 480.0f, -1.0f, 1.0f); //Create an orthographic matrix
     glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0));
-    glm::vec3 translation = glm::vec3(100.0f, 100.0f, 0);
 
     Texture* texture = new Texture("res/textures/colourSplash.png");
     Shader* shader = new Shader("res/shaders/basic.shader"); //Create a new shader    
@@ -89,20 +87,15 @@ int main(void)
     
     Renderer* renderer = new Renderer(); //Create a new render to make draw calls
 
-    ImGui::CreateContext();
-    ImGui::StyleColorsDark();
 
+    //ImGui instantiantion
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark(); //Dark mode B-)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
-
-
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    float increment = 0.05f;
-    float r = 0.0f;
+    glm::vec3 translation1 = glm::vec3(100.0f, 100.0f, 0); //Translation for image 1
+    glm::vec3 translation2 = glm::vec3(300.0f, 100.0f, 0); //Translation for image 2
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -114,22 +107,27 @@ int main(void)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        if (r > 1.0f || r < 0.0f)
-            increment *= -1;
-        r += increment;
-
-        ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 680.0f);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 MVP = proj * view * model;
+        ImGui::SliderFloat3("Translation1", &translation1.x, 0.0f, 680.0f); //Set the translation for image 1
+        ImGui::SliderFloat3("Translation2", &translation2.x, 0.0f, 680.0f); //Set the translation for image 2
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate); //Output framerate
 
         va->Bind();
         ib->Bind();
         texture->Bind();
         shader->Bind();
+
+
+        
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation1);
+        glm::mat4 MVP = proj * view * model; //Move image 1 to translated spot
         shader->SetUniformMat4f("u_MVP", MVP);
-        renderer->Draw(*va, *ib, *shader);
+        renderer->Draw(*va, *ib, *shader); //Draw image 1
+        
+
+        model = glm::translate(glm::mat4(1.0f), translation2);
+        MVP = proj * view * model;
+        shader->SetUniformMat4f("u_MVP", MVP); //Move image 2 to translated spot
+        renderer->Draw(*va, *ib, *shader); //Draw image 2
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -149,8 +147,8 @@ int main(void)
     delete vb;
     delete ib;
 
-    ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
     ImGui::DestroyContext();
     glfwTerminate();
     return 0;
